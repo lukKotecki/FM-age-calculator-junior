20e106454c68c79b41e87f4d053a138924aefbd4
Lukas <lukkotecki@gmail.com>
2024-02-04 12:43:04 +0100

    before change units

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
 
-let container = document.getElementsByClassName('container')[0];
+const container = document.getElementsByClassName('container')[0];
+
+const dayInput = document.getElementById('dayInput')
+const monthInput = document.getElementById('monthInput')
+const yearInput = document.getElementById('yearInput')
+const submitButton = document.getElementById('submitButton')
+const dateForm = document.getElementById('date-form')
+
+const yearsOutput = document.getElementById('years-output')
+const monthsOutput = document.getElementById('months-output')
+const daysOutput = document.getElementById('days-output')
 
 
+submitButton.addEventListener('click', (e) =>{
+  e.preventDefault()
+  handleSubmit()
+  // console.log('kliknieto')
+})
+
+// dateForm.addEventListener('submit', (e) =>{
+//   e.preventDefault()
+//   handleSubmit()
+//   console.log('kliknieto')
+// })
+
+
+
+function handleSubmit(){
+    const day = dayInput.value
+    const month = monthInput.value
+    const year = yearInput.value
+    const dateObject = {year,month,day}
+    
+    if(isDateValid(dateObject)){
+        console.log('Data nie jest prawidłowa')
+
+
+    }
+
+    console.log(dateObject)
+
+}
+
+function isDateValid(dateObject){
+
+
+    return dateObject
+}
+
+
+
+function checkPastDate(stringDate){
+const inputDate = new Date(stringDate); 
+const currentDate = new Date();
+if (inputDate < currentDate) {
+  console.log('The input date is in the past.');
+} else {
+  console.log('The input date is in the future.');
+}
+}
